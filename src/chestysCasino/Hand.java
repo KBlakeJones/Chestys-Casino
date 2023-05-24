c458ef41672a7b51bfa008b37243a7ffc499cb3b
KBlakeJones <119406401+KBlakeJones@users.noreply.github.com>
2023-05-24 18:35:54 -0400

    Update 5/24

--- src/chestysCasino/Hand.java
+++ src/chestysCasino/Hand.java
@@ -1,5 +1,47 @@
 package chestysCasino;
 
-public class Hand {
+public class Hand { 
+	
+	private int cards;
+	private int value;
+	
+	public Hand() {
+		//collection of cards
+	}
+	
+	
+	public void hit() {
+		//add deck.next();  ???
+	}
+	
+	public void stay() {
+		// move to next hand in order
+	}
+	
+	public void split() {
+		// create new hand with each card
+	}
+
+	public int getCards() {//do we need net next for random card
+		return cards;
+	}
+	
+	public int calculateValue() {
+		return value;
+	}
+	
+//	public void doubleDown() {
+//		bet *= 2;
+//	}
+	//doubleDown bet *=2; here or in 
+
+	public Hand(int cards) {
+		super();
+		this.cards = cards;
+	}
+	
+	
+	
+	
 
 }
